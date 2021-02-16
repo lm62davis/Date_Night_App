97ac7f56124b43f93be909a4690a160dda9e8d60
bbish <brenana88@gmail.com>
2021-02-16 18:17:21 -0500

    movie dropdown and generate button created

--- JS/HomeView.js
+++ JS/HomeView.js
@@ -1,14 +1,50 @@
 const movieElement = function(movies){
     
 
-    const mainElement = document.createElement("main");
+    const mainElement = document.createElement("div");
+    mainElement.classList.add("movie-container")
 
-  // const test = document.createElement("section")
-    movies.movie_results.forEach(movie => {
-    const title = document.createElement("section");
-    title.innerText = movie.title
-    mainElement.appendChild(title)
-    });
+    const movieDropdownLabel = document.createElement("select")
+    movieDropdownLabel.classList.add("movie-dropdown-label")
+    mainElement.appendChild(movieDropdownLabel)
+    
+    const defaultGenreDropdown = document.createElement("option")
+    defaultGenreDropdown.selected = true
+    defaultGenreDropdown.innerText = "Select a genre"
+    movieDropdownLabel.appendChild(defaultGenreDropdown)
+
+    const musicGenreDropdown = document.createElement("option")
+    musicGenreDropdown.innerText = "Music"
+    const horrorGenreDropdown = document.createElement("option")
+    horrorGenreDropdown.innerText = "Horror"
+    const thrillerGenreDropdown = document.createElement("option")
+    thrillerGenreDropdown.innerText = "Thriller"
+    const mysteryGenreDropdown = document.createElement("option")
+    mysteryGenreDropdown.innerText = "Mystery"
+    
+    movieDropdownLabel.appendChild(musicGenreDropdown)
+    movieDropdownLabel.appendChild(horrorGenreDropdown)
+    movieDropdownLabel.appendChild(thrillerGenreDropdown)
+    movieDropdownLabel.appendChild(mysteryGenreDropdown)
+
+
+    //horror, thriller, mystery, music
+
+
+    
+
+    
+    const movieButton = document.createElement("button")
+    movieButton.innerText = "Generate"
+    mainElement.appendChild(movieButton)
+
+    
     return mainElement
 } 
 export {movieElement};
+
+// movies.movie_results.forEach(movie => {
+    // const title = document.createElement("section");
+    // title.innerText = movie.title
+    // mainElement.appendChild(title)
+    // });
