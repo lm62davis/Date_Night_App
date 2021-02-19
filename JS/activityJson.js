674c8445468c38fc96ea941f605da5037c9c111f
mandyjohnston <amandamass03@gmail.com>
2021-02-19 09:06:20 -0500

    Activity section functionality

---
+++ JS/activityJson.js
@@ -0,0 +1,35 @@
+const activityJSON = [
+    {
+        name: "Board Game",
+        description: "Take out the monopoly board and play the night away!",
+        id: 1,
+        type: "indoor",
+        loveLanguage: "Quality Time"
+    },
+
+    {
+        name: "Paint & Sip",
+        description: "Pick out a picture to portray, and your favorite bottle of wine!",
+        id: 2,
+        type: "indoor",
+        loveLanguage: "Quality Time"
+      },  
+
+      {
+        name: "Take a hike",
+        description: "Go to your favorite park and take in nature together.",
+        id: 3,
+        type: "outdoor",
+        loveLanguage: "Quality Time"
+      },
+      {
+        name: "Go to the beach",
+        description: "Bask in the sun and take a swim together",
+        id: 4,
+        type: "outdoor",
+        loveLanguage: "Quality Time"
+      }
+    ]
+
+    export {activityJSON};
+
--- JS/app.js
+++ JS/app.js
@@ -2,6 +2,8 @@ import {homeElement} from "./HomeView.js"
 import {createHeader} from "./header.js"
 import {createFooter} from "./footer.js"
 import {foodSection} from "./food-section.js"
+import {activitySection} from "./activityView.js"
+import { activityJSON } from "./activityJson.js"
 
 const clearChildren = function (element){
     while(element.firstChild){
@@ -30,6 +32,8 @@ const displayHomeView = function(movies, food) {
     container.appendChild(mainElement);
 	let main2Element = foodSection(food);
 	container.appendChild(main2Element)
+	let main3Element = activitySection(activityJSON);
+	container.appendChild(main3Element);
     let footer = createFooter();
     container.appendChild(footer);
 }
