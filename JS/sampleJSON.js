4e4d5fedffe3c481491f2701017ed51845e64b67
Linda Martinez Davis <lm62davis@outlook.com>
2021-02-21 22:44:01 -0500

    Slot Machine Title added

---
+++ JS/sampleJSON.js
@@ -0,0 +1,15 @@
+Promise.all([
+	fetch('https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-random-movies&page=1'),
+	fetch('https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-random-movies&page=1')
+]).then(function (responses) {
+	// Get a JSON object from each of the responses
+	return Promise.all(responses.map(function (response) {
+		return response.json();
+	}));
+}).then(function (data) {
+	// Log the data to the console
+	// You would do something with both sets of data here
+	console.log(data);
+}).catch(function (error) {
+	console.log(error);
+});
