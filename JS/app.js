45953e9dc5e1462e8581b325b507dddbdd9dc728
bbish <brenana88@gmail.com>
2021-02-25 11:54:13 -0500

    link views

--- JS/app.js
+++ JS/app.js
@@ -11,48 +11,8 @@ const clearChildren = function (element){
     }
 }
 
-const container = document.querySelector(".main-content");
-
-let movieJson = "";
-let movieJson2 = "";
-let movieJson3 = "";
-let allMovies = [];
-let foodJson = "";
-let foodJson2 = "";
-let foodJson3 = "";
-let allFoods = "";
-
-
-const checkIfAllLoaded = function() {
-	if(movieJson != "" && foodJson != "" && movieJson2 != "" && movieJson3 != "" && foodJson2 != "" && foodJson3 != "") {
-		allMovies = movieJson;
-		allFoods = foodJson;
-		movieJson2.movie_results.forEach(movie => {
-			allMovies.movie_results.push(movie)
-		});
-		movieJson3.movie_results.forEach(movie => {
-			allMovies.movie_results.push(movie)
-		});
-		foodJson2.result.data.forEach(cuisine => {
-			allFoods.result.data.push(cuisine)
-		});
-		foodJson3.result.data.forEach(cuisine => {
-			allFoods.result.data.push(cuisine)
-		});
-		displayHomeView (allMovies, allFoods)
-		console.log(movieJson);
-		console.log(movieJson2)
-		console.log(movieJson3)
-		console.log(allMovies)
-		console.log(foodJson);
-		console.log(foodJson2)
-		console.log(foodJson3)
-		console.log(allFoods);
-
-	}
-}
-
 const displayHomeView = function(movies, food) {
+	const container = document.querySelector(".display-content");
     clearChildren(container);
     let header = createHeader();
     container.prepend(header);
@@ -66,138 +26,13 @@ const displayHomeView = function(movies, food) {
     container.appendChild(footer);
 }
 
-// let multipleMovieLists = []; 
-
-
-fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-random-movies&page=1", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "603d5c585dmsh8a5028e09f6a248p1a9c3bjsn8df290ecfe4f",
-		"x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com"
-	}
-	
-})
-
-.then((response) => response.json())
-.then((movies) => {
-	movieJson= movies
-	checkIfAllLoaded()
-})
-.catch(err => {
-	console.error(err);
-});
-// multipleMovieLists.push(movies.value)
-// console.log(multipleMovieLists)
-
-
-
-fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-random-movies&page=2",  {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "603d5c585dmsh8a5028e09f6a248p1a9c3bjsn8df290ecfe4f",
-		"x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com"
-	}
-	
-})
-
-.then((response) => response.json())
-.then((movies) => {
-	movieJson2= movies
-	checkIfAllLoaded()
-})
-.catch(err => {
-	console.error(err);
-});
-
-fetch("https://movies-tvshows-data-imdb.p.rapidapi.com/?type=get-random-movies&page=3",  {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "603d5c585dmsh8a5028e09f6a248p1a9c3bjsn8df290ecfe4f",
-		"x-rapidapi-host": "movies-tvshows-data-imdb.p.rapidapi.com"
-	}
-	
-})
-
-.then((response) => response.json())
-.then((movies) => {
-	movieJson3= movies
-	checkIfAllLoaded()
-})
-.catch(err => {
-	console.error(err);
-});
-
-
-
-// fetch("https://us-restaurant-menus.p.rapidapi.com/restaurants/zip_code/90210?page=1", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "c03c7ea319mshe450b817bc71590p1d6478jsn9e3847b68620",
-// 		"x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com"
-// 	}
-// })
-// .then(response => response.json()) 
-// .then((food) => displayHomeView(food))	
-// .catch(err => {
-// 	console.error(err);
-// });
-
-
-fetch("https://us-restaurant-menus.p.rapidapi.com/restaurants/state/OH?page=1", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "c03c7ea319mshe450b817bc71590p1d6478jsn9e3847b68620",
-		"x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com"
-	}
-})
-.then(response => response.json()) 
-.then((food) => {
-	foodJson=food
-	checkIfAllLoaded()
-})	
-.catch(err => {
-	console.error(err);
-});
-
-fetch("https://us-restaurant-menus.p.rapidapi.com/restaurants/state/OH?page=2", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "c03c7ea319mshe450b817bc71590p1d6478jsn9e3847b68620",
-		"x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com"
-	}
-})
-.then(response => response.json()) 
-.then((food) => {
-	foodJson2=food
-	checkIfAllLoaded()
-})	
-.catch(err => {
-	console.error(err);
-});
-
-fetch("https://us-restaurant-menus.p.rapidapi.com/restaurants/state/OH?page=3", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "c03c7ea319mshe450b817bc71590p1d6478jsn9e3847b68620",
-		"x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com"
-	}
-})
-.then(response => response.json()) 
-.then((food) => {
-	foodJson3=food
-	checkIfAllLoaded()
-})	
-.catch(err => {
-	console.error(err);
-});
-
 
 
 export { clearChildren}
+export { displayHomeView}
 
 
-
-/// <reference path="../typings/tsd.d.ts" />
+// / <reference path="../typings/tsd.d.ts" />
 
 
 
---
+++ JS/contactView.js
@@ -0,0 +1,39 @@
+const brenaElement = document.createElement("section")
+const brenaNameElement = document.createElement("h2")
+brenaNameElement.innerText="Brena Bishop"
+const brenaPortfolio = document.createElement("a")
+brenaPortfolio.href = "https://brenabishop.github.io/"
+brenaElement.appendChild(brenaNameElement)
+brenaElement.appendChild(brenaPortfolio)
+
+const brandonElement = document.createElement("section")
+const brandonNameElement = document.createElement("h2")
+brandonNameElement.innerText = "Brandy Terry"
+const brandonPortfolio = document.createElement("a")
+brandonPortfolio.href = "https://brandonterry.github.io/"
+brandonElement.appendChild(brandonNameElement)
+brenaElement.appendChild(brandonPortfolio)
+
+const sonyaElement = document.createElement("section")
+const sonyaNameElement = document.createElement("h2")
+sonyaNameElement.innerText = "Sonya Stewart"
+const sonyaPortfolio = document.createElement("a")
+sonyaPortfolio.href = "https://sonyastewart.github.io/"
+sonyaElement.appendChild(sonyaNameElement)
+sonyaElement.appendChild(sonyaPortfolio)
+
+const lindaElement = document.createElement("section")
+const lindaNameElement = document.createElement("h2")
+lindaNameElement.innerText = "Linda Martinez Davis"
+const lindaPortfolio = document.createElement("a")
+lindaPortfolio.href = "https://lindamdavis.github.io/"
+lindaElement.appendChild(lindaNameElement)
+lindaElement.appendChild(lindaPortfolio)
+
+const mandyElement = document.createElement("section")
+const mandyNameElement = document.createElement("h2")
+mandyNameElement.innerText = "Mandy Johnston"
+const mandyPortfolio = document.createElement("a")
+mandyPortfolio.href = "https://mandyjohnston.github.io/"
+mandyElement.appendChild(mandyNameElement)
+mandyElement.appendChild(mandyPortfolio)
